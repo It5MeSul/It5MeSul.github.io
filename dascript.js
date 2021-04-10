e37874ea325c97fb5e123671c6d429b6c535f9dd
Seth Fernando <68430270+sethfernando@users.noreply.github.com>
2021-04-10 19:58:52 +0530

    Updated webpage to new style.

---
+++ dascript.js
@@ -0,0 +1,9 @@
+var d = prompt("Welcome to Sulani's Poems. You are currently on a Computer. Type the name of this website to prove you are a human.");
+if(d == "suls-poems.sethestudio.com"){
+	alert("Correct. You may proceed");
+}else{
+	alert("You are wrong. And you are being reported to this website's owner!!!!!!");
+	while(true){
+		alert("You may not enter!!!!!!!!");
+	}
+}
